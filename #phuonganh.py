acf5f19c47dca927d1b4fb49d9597748ff1f512d
23174600063 <148171947+23174600063@users.noreply.github.com>
2023-10-30 21:11:25 +0700

    Add files via upload

---
+++ #phuonganh.py
@@ -0,0 +1,35 @@
+# 1.2
+x=10
+y=5
+print ("tong",x+y)
+print ("hieu",x-y)
+print ("tich",x*y)
+print ("thuong",x/y)
+
+ 
+# 1.3
+soluong=5
+dongia=25000
+thanhtien=soluong*dongia
+print("ten hang : sua hop vinamilk")
+print("so luong" ,soluong)
+print("don gia" ,dongia)
+print("thanh tien" ,thanhtien)
+
+
+#1.4
+import math
+a=float(input("4"))
+b=float(input("5"))
+c=float(input("6"))
+p=(a+b+c)/2
+s=math.sqrt(p*(p-a)*(p-b)*(p-c))
+print("diện tích tam giác là:",s)
+
+
+# 1.1
+print("**    **  ******  **      **      ******")
+print("**    **  **      **      **      **  **")
+print("********  ******  **      **      **  **")
+print("**    **  **      **      **      **  **")
+print("**    **  ******  ******  ******  ******")
